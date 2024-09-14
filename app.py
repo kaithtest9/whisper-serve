40dfc1be320681832a5f69fe4497003c9743834e
salamer <salamer_gaga@163.com>
2024-09-14 16:19:35 +0800

    fix

--- app.py
+++ app.py
@@ -6,5 +6,22 @@ app = Flask(__name__)
 def home():
     return 'Hello, World!'
 
+@app.route('/audio', methods=['POST'])
+def audio():
+    return jsonify({
+        'transcript': 'Hello, World!'
+    })
+
+@app.route('/transcript', methods=['POST'])
+def transcript():
+    jfk_path = "jfk.flac"
+    model = WhisperModel("tiny", device="cpu", compute_type="int8", download_root="/app")
+    segments, info = model.transcribe(jfk_path, word_timestamps=True)
+    resp = ''
+    for segment in segments:
+        print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+        resp += f"[%.2fs -> %.2fs] {segment.text}\n" % (segment.start, segment.end)
+    return resp
+
 if __name__ == '__main__':
     app.run(debug=True)
