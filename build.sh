144c15da7501c6f3a536f7725e2df1f0478346ad
salamer <salamer_gaga@163.com>
2024-09-15 17:57:30 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 pip install faster-whisper
 pip install flask gunicorn
 apt-get update && apt-get install -y git
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 pip install git+https://github.com/m-bain/whisperx.git
 python load_model.py
